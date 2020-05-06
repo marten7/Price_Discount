73c734034a39fea601c5e02c6cfe16fb461c4316
wushuangjie <wushuangjie@haima.me>
2020-05-06 14:46:29 +0800

    fix bug

--- cgi-bin/price.py
+++ cgi-bin/price.py
@@ -20,8 +20,9 @@ shop_price3 = f.getvalue('shopPrice3')
 shop_offer3 = f.getvalue('shopOffer3')
 net_offer = f.getvalue('netOffer')
 
-sum_price = shop_price1 + shop_price2 + shop_price3
-sum_offer = shop_offer1 + shop_offer2 + shop_offer3 + net_offer
+
+sum_price = float(shop_price1) + float(shop_price2) + float(shop_price3)
+sum_offer = float(shop_offer1) + float(shop_offer2) + float(shop_offer3) + float(net_offer)
 real_sum = sum_price - sum_offer
 
 print ("Content-type:text/html")
@@ -36,8 +37,8 @@ print (u"<h2> 店铺1价格：%s,店铺优惠： %s</h2>" % (shop_price1, shop_o
 print (u"<h2> 店铺2价格：%s,店铺优惠： %s</h2>" % (shop_price2, shop_offer2))
 print (u"<h2> 店铺3价格：%s,店铺优惠： %s</h2>" % (shop_price3, shop_offer3))
 print (u"<h2> 全网优惠：%s</h2>" % net_offer)
-print (u"<h2> h2商品未优惠总价：%s</h2>" % sum_price)
-print (u"<h2> 商品总优惠：%s</h2>" % sum_offer)
-print (u"<h2> 商品实际总价：%s</h2>" % real_sum)
+print (u"<h2> h2商品未优惠总价：%d</h2>" % sum_price)
+print (u"<h2> 商品总优惠：%d</h2>" % sum_offer)
+print (u"<h2> 商品实际总价：%d</h2>" % real_sum)
 print ("</body>")
 print ("</html>")
